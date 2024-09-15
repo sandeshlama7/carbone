7a0943992c01551ecd37f301d229fcafd8c4ed79
Sandesh Lama <sandeshislama7@gmail.com>
2024-09-15 22:26:25 +0545

    Changed logic for checking whether to create a vpc

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -9,17 +9,21 @@ locals {
   project_prefix = var.project_prefix
 
   ##### VPC
-  create_igw           = true
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
-  enable_flow_log      = false
 
   vpc = {
-    name = module.naming.resources.vpc.name
-    cidr = var.vpc_cidr
-    azs  = slice(data.aws_availability_zones.available.names, 0, var.number_of_azs)
+    create_vpc           = var.vpc_id != "" && length(var.private_subnets_id) != 0 && length(var.public_subnets_id) != 0 ? false : true
+    name                 = module.naming.resources.vpc.name
+    cidr                 = var.vpc_cidr
+    azs                  = slice(data.aws_availability_zones.available.names, 0, var.number_of_azs)
+    create_igw           = true
+    enable_dns_hostnames = true
+    enable_dns_support   = true
+    enable_nat_gateway   = true
+    single_nat_gateway   = true
+    enable_flow_log      = false
+    number_of_azs        = var.number_of_azs
   }
-  number_of_azs = var.number_of_azs
+  vpc_id = local.vpc.create_vpc != true ? var.vpc_id : module.vpc.vpc_id
+  public_subnets_id = local.vpc.create_vpc != true ? var.public_subnets_id : module.vpc.public_subnets
+  private_subnets = local.vpc.create_vpc != true ? var.private_subnets_id : module.vpc.private_subnets
 }
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -2,7 +2,7 @@ module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name             = local.vpc.name
-  create_vpc       = var.vpc_id != "" && length(var.private_subnets_id) != 0 && length(var.public_subnets_id) != 0 ? false : true
+  create_vpc       = local.vpc.create_vpc
   cidr             = local.vpc.cidr
   azs              = local.vpc.azs
   private_subnets  = [for k, v in local.vpc.azs : cidrsubnet(local.vpc.cidr, 4, k)]
@@ -10,13 +10,13 @@ module "vpc" {
   database_subnets = [for k, v in local.vpc.azs : cidrsubnet(local.vpc.cidr, 4, k + 4)]
 
   # Gateways
-  create_igw         = local.create_igw
-  enable_nat_gateway = local.enable_nat_gateway
-  single_nat_gateway = local.single_nat_gateway
+  create_igw         = local.vpc.create_igw
+  enable_nat_gateway = local.vpc.enable_nat_gateway
+  single_nat_gateway = local.vpc.single_nat_gateway
 
-  enable_dns_hostnames = local.enable_dns_hostnames
-  enable_dns_support   = local.enable_dns_support
+  enable_dns_hostnames = local.vpc.enable_dns_hostnames
+  enable_dns_support   = local.vpc.enable_dns_support
 
   #VPC Flow Log
-  enable_flow_log = local.enable_flow_log
+  enable_flow_log = local.vpc.enable_flow_log
 }

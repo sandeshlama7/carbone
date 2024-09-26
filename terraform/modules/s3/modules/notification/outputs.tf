8d3ffc9c0ff5a7495fe8913ecf2363e8ae17d87a
Sandesh Lama <sandeshislama7@gmail.com>
2024-09-26 12:40:15 +0545

    Removed unused s3 module

--- terraform/modules/s3/modules/notification/outputs.tf
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Defines the list of output for the created infrastructure
-################################################################################
-
-output "s3_bucket_notification_id" {
-  description = "ID of S3 bucket"
-  value       = try(aws_s3_bucket_notification.this[0].id, "")
-}
